922171b238897092173262c3174fdb1d732394cf
Yash Mishra <64361746+Yash15-maker@users.noreply.github.com>
2021-09-06 21:04:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Developer", "Designer", "Freelancer"],
+    strings: ["Developer", "Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
